4f789533bf63606049349ca45f170bc656c8c0b4
vincianenahimana <vnahimana@gmail.com>
2025-02-17 12:41:03 +0100

    add filter in the API

--- backend/controllers/geoController.mjs
+++ backend/controllers/geoController.mjs
@@ -1,16 +1,35 @@
-import { getGeoData } from "../models/geoModel.mjs";
+import { getGeoData, getFiveFirstData } from "../models/geoModel.mjs";
 import { formatToGeoJSON } from "../utils/geoUtils.mjs";
 
 export async function fetchGeoData(req, res) {
     try {
-        const data = await getGeoData();
+
+        const { type_entite } = req.query;
+
+        console.log('Filtrage avec type_entite:', type_entite);
+
+        if (!type_entite) {
+            return res.status(400).json({ error: "'type_entite' required" });
+        }
+
+        const data = await getGeoData(type_entite);
         const dataFormatted = formatToGeoJSON(data);
 
         res.status(200).json(dataFormatted)
-
 
     } catch (error) {
         console.error('Error during fetching geodata', error);
         res.status(500).json({ error: 'Internal Server Error.' });
     }
+}
+
+export async function fetchFiveFirstData(req,res) {
+    try {
+        const data = await getFiveFirstData()
+        res.status(200).json({data})
+    } catch (error) {
+        console.error('Error during fetching fivedata', error);
+        res.status(500).json({ error: 'Internal Server Error.' });
+    }
+    
 }
--- backend/models/geoModel.mjs
+++ backend/models/geoModel.mjs
@@ -1,5 +1,5 @@
 import client from "../config/db.mjs";
-export async function getGeoData() {
+export async function getGeoData(typeEntite) {
   const query = `
      SELECT 
         ogc_fid, 
@@ -10,14 +10,35 @@ export async function getGeoData() {
         taux_de_chomage_administratif_des_15_64_ans,
         taux_de_chomage_administratif_des_hommes_de_15_64_ans,
         taux_de_chomage_administratif_des_femmes_de_15_64_ans,
-        taux_de_chomage_administratif_des_15_24_ans,
-        taux_de_chomage_administratif_des_25_49_ans,
-        taux_de_chomage_administratif_des_50_64_ans,
-        taux_de_chomage_administratif_des_15_19_ans,
         type_et_entite,
         ST_AsGeoJSON(wkb_geometry) AS geojson
     FROM taux_chomage_15_64
-    LIMIT 2
+    WHERE type_entite = $1
+    `;
+
+  try {
+    const response = await client.query(query,[typeEntite]);
+    const results = response.rows;
+    return results;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getFiveFirstData() {
+  const query = `
+     SELECT 
+        ogc_fid, 
+        ins, 
+        type_entite, 
+        entite, 
+        taux_de_chomage_administratif_des_15_64_ans,
+        taux_de_chomage_administratif_des_hommes_de_15_64_ans,
+        taux_de_chomage_administratif_des_femmes_de_15_64_ans,
+        type_et_entite
+    FROM taux_chomage_15_64
+    ORDER BY taux_de_chomage_administratif_des_15_64_ans DESC
+    LIMIT 5;
     `;
 
   try {
