607dd0e4855e097ecc08fb84e868269650ac515a
vincianenahimana <vnahimana@gmail.com>
2025-02-19 12:31:31 +0100

    modify the result's controller

--- backend/controllers/geoController.mjs
+++ backend/controllers/geoController.mjs
@@ -1,10 +1,10 @@
-import { getGeoData, getFiveFirstData } from "../models/geoModel.mjs";
+import { getGeoData} from "../models/geoModel.mjs";
 import { formatToGeoJSON } from "../utils/geoUtils.mjs";
 
 export async function fetchGeoData(req, res) {
     try {
 
-        const { type_entite } = req.query;
+        const { type_entite} = req.query;
 
         console.log('Filtrage avec type_entite:', type_entite);
 
@@ -13,23 +13,17 @@ export async function fetchGeoData(req, res) {
         }
 
         const data = await getGeoData(type_entite);
-        const dataFormatted = formatToGeoJSON(data);
+        
+        const dataFormatted = formatToGeoJSON(data.results);
 
-        res.status(200).json(dataFormatted)
+        res.status(200).json({
+            dataFormatted,
+            entityMaxPercent : data.maxEntity,
+            entityMinPercent: data.minEntity     
+        })
 
     } catch (error) {
         console.error('Error during fetching geodata', error);
         res.status(500).json({ error: 'Internal Server Error.' });
     }
-}
-
-export async function fetchFiveFirstData(req,res) {
-    try {
-        const data = await getFiveFirstData()
-        res.status(200).json({data})
-    } catch (error) {
-        console.error('Error during fetching fivedata', error);
-        res.status(500).json({ error: 'Internal Server Error.' });
-    }
-    
 }
