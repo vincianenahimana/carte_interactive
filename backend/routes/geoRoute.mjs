29d5874ee81269a492cbd6dcbe5d1a41362dcc4d
vincianenahimana <vnahimana@gmail.com>
2025-02-19 12:32:04 +0100

    remove a router

--- backend/routes/geoRoute.mjs
+++ backend/routes/geoRoute.mjs
@@ -1,10 +1,9 @@
 import express from 'express';
-import { fetchGeoData,fetchFiveFirstData } from '../controllers/geoController.mjs';
+import { fetchGeoData} from '../controllers/geoController.mjs';
 
 const router = express.Router();
 
 router.get('/api/geodata', fetchGeoData)
-router.get('/api/data-toprank',fetchFiveFirstData)
 
 export default router;
 
